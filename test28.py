7cc763a02bec143ef649efaf68758c889e5b3e01
anjulanadeeshan <mranjula660@gmail.com>
2026-07-06 22:04:53 +0530

    Uoj coders procatice day 5

--- test28.py
+++ test28.py
@@ -1,4 +1,17 @@
-ar = []
-for i in range(3):
-    ar = list(map(int, input().rstrip().split()))
+from collections import Counter
 
+def subarr(ar) :
+    counts = Counter(ar)
+    max_len = 0
+    for v in counts:
+        length = counts[v] + counts[v+1]
+        max_len = max(max_len, length)
+
+    return(max_len)
+        
+        
+
+n = int(input())
+ar = list(map(int, input().split()))
+
+print(subarr(ar))
